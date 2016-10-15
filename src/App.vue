fad57ae35623b47286a3b68b18c4226f683dfeeb
MortalWombat42 <alexiv42@gmail.com>
2016-10-14 23:33:46 -0400

    Fuckin around
    dont know what im doin

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <div id="app">
+  <div id="app" class="cm-sm-6">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      msg: 'Hello Vue!'
+  export default {
+    data () {
+      return {
+        msg: 'Dank Memes!',
+      }
     }
   }
-}
 </script>
 
 <style>
