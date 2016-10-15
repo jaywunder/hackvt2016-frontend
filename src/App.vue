401b15e9f05c9b0dc33a59b31ae77b4ef22f14d9
QuadraticKing <zave456@gmail.com>
2016-10-14 23:33:59 -0400

    Slider
    This is a Slider, not global yet

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
+    <!-- <img src="./assets/logo.png"> -->
+    <h1>{{ myMessage }}</h1>
+
+    <!-- <div>
+      <input v-model="parentMsg">
+      <br>
+      <child v-bind:my-message="parentMsg"></child>
+    </div> -->
+
+    <slider name="Albany" value="25" min="0" max="1000"/>
+    <!-- <child :value="value">f</child>
+    <h2>Fuuuuuun</h2> -->
+
   </div>
 </template>
 
 <script>
+import Slider from './components/Slider.vue'
+
 export default {
-  data () {
+  data() {
     return {
-      msg: 'Hello Vue!'
+      myMessage: 'Hello Vue!',
     }
+  },
+  components: {
+    'slider': Slider
   }
 }
 </script>
---
+++ src/components/Slider.vue
@@ -0,0 +1,27 @@
+<template>
+  <div is="slider">
+    <h2>{{ name }} <input type="range" v-bind:id="name" v-bind:min="min" v-bind:max="max" v-model="value"> {{value}} </input> </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['name', 'value', 'min', 'max'],
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    printValue: function () {
+
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
