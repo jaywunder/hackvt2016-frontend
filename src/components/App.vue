847f2c6caa13e9ee57582419ec60dcc7eefb6cc5
Dillon Beliveau <Dillon.Beliveau@dealertrack.com>
2016-10-15 08:42:26 -0400

    kinda nice looking boxes

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,9 +1,50 @@
 <template>
   <div id="wrapper">
+    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.html">Di$tributr</a>
+      </div>
+    </nav>
     <div id="app" class="container">
-      <img src="../assets/logo.png">
-      <h1>{{ msg }}</h1>
-      <vermont/>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              <div class="row">
+                <div class="col-xs-3">
+                  <i class="fa fa-comments fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                  <div class="huge">EDIT</div>
+                  <div>Your shit!</div>
+                </div>
+              </div>
+            </div>
+            Butt
+          </div>
+        </div>
+        <div class="col-md-9">
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              <div class="row">
+                <div class="col-xs-3">
+                  <i class="fa fa-comments fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                  <div class="huge">METRICS</div>
+                  <div>Go here!</div>
+                </div>
+              </div>
+            </div>
+            <vermont/>
+          </div>
+        </div>
     </div>
     </div>
 </template>
