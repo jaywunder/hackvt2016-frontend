830e13af4a53d335936a2aacd0f5d6be07814027
jaywunder <jaywunder@gmail.com>
2016-10-14 23:38:45 -0400

    added Vermont element

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,21 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
+    <vermont></vermont>
   </div>
 </template>
 
 <script>
+import Vermont from './components/Vermont.vue'
+
 export default {
   data () {
     return {
       msg: 'Hello Vue!'
     }
+  },
+  components: {
+    'vermont': Vermont
   }
 }
 </script>
